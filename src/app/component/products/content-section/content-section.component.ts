d6add1607c1a4724845b2c2ae4a4a7eb4a9dedfb
abdo <abdoalaaabdo17@gmail.com>
2023-05-15 07:45:53 +0300

    1.2.4

--- src/app/component/products/content-section/content-section.component.ts
+++ src/app/component/products/content-section/content-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DiscountOffers } from 'src/app/models/discount-offers';
 import { IProduct } from 'src/app/models/iproduct';
 import { Store } from 'src/app/models/store';
@@ -444,5 +444,74 @@ export class ContentSectionComponent implements OnInit {
       );
     }
     return this.productsList;
+  }
+
+  // @Output() newCartProductEvent: EventEmitter<IProduct[]> = new EventEmitter<IProduct[]>();
+  // @Output() newCartQuantityEvent: EventEmitter<any[]> = new EventEmitter<any[]>();
+
+  // cartProduct: IProduct[] = [];
+  // cartQuantity: any[] = [
+  //   {
+  //     id: 0,
+  //     quantity: 0,
+  //   },
+  // ];
+
+  // addToCart(product: IProduct) {
+  //   if (this.cartProduct.includes(product)) {
+  //     this.cartQuantity.forEach((element) => {
+  //       if (element.id == product.id) {
+  //         element.quantity++;
+  //       }
+  //     });
+  //   } else {
+  //     this.cartProduct.push(product);
+  //     let object = {
+  //       id: product.id,
+  //       quantity: 1,
+  //     };
+  //     this.cartQuantity.push(object);
+  //   }
+  //   // console.log(this.cartProduct);
+  //   // console.log(this.cartQuantity);
+
+  //   this.newCartProductEvent.emit(this.cartProduct);
+  //   this.newCartQuantityEvent.emit(this.cartQuantity);
+  // }
+
+  cartProduct: IProduct[] = [];
+  addToCart(product: any): void {
+    this.cartProduct = product;
+  }
+
+  cartQuantity: any[] = [];
+  addQuantity(quantity: any): void {
+    this.cartQuantity = quantity;
+  }
+
+  getQuantity(product: IProduct): number {
+    let value = 0;
+    this.cartQuantity.forEach((element) => {
+      if (element.id === product.id) {
+        value = element.quantity;
+      }
+    });
+    return value;
+  }
+
+  removeProduct(product: IProduct): void {
+    let index = this.cartProduct.indexOf(product);
+    console.log(this.cartProduct.indexOf(product));
+    if (index != -1) {
+      this.cartQuantity.forEach((element) => {
+        if (element.id == product.id) {
+          if (element.quantity > 1) {
+            element.quantity = element.quantity - 1;
+          } else {
+            this.cartProduct.splice(index, 1);
+          }
+        }
+      });
+    }
   }
 }
